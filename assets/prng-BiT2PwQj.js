d3f504ded6df76336432e36ed85b3009e3facbf1
jbatterson <jason.batterson@gmail.com>
2026-04-10 20:28:34 -0700

    Updates

---
+++ assets/prng-BiT2PwQj.js
@@ -0,0 +1,1 @@
+function u(e){return function(){let t=e+=1831565813;return t=Math.imul(t^t>>>15,t|1),t^=t+Math.imul(t^t>>>7,t|61),((t^t>>>14)>>>0)/4294967296}}function n(e){let r=2166136261;for(let t=0;t<e.length;t++)r^=e.charCodeAt(t),r=Math.imul(r,16777619);return r>>>0}export{n as h,u as m};
